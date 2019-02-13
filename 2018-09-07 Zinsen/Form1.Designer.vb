3dda89dd4bc8e3d846376b9c4bebf883549e1901
Benedikt Michael Schneider <bs762448@fh-muenster.de>
2019-02-13 11:04:37 +0100

    Projektdateien hinzufügen.

---
+++ 2018-09-07 Zinsen/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmdZinsen = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmdZinsen
+        '
+        Me.cmdZinsen.Location = New System.Drawing.Point(161, 104)
+        Me.cmdZinsen.Name = "cmdZinsen"
+        Me.cmdZinsen.Size = New System.Drawing.Size(334, 262)
+        Me.cmdZinsen.TabIndex = 0
+        Me.cmdZinsen.Text = "Zinsen"
+        Me.cmdZinsen.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cmdZinsen)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents cmdZinsen As Button
+End Class
---
+++ 2018-09-07 Zinsen/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub cmdZinsen_Click(sender As Object, e As EventArgs) Handles cmdZinsen.Click
+        Dim mZinssatz As Single, mP As Single, mK As Single, mZ As Single
+        Const conStartK = 1000
+        mZinssatz = InputBox("Zinssatz?")
+        mP = Val(mZinssatz)
+        mK = conStartK
+        Do
+            MsgBox(mK)
+            mZ = mK * mP / 100
+            mK = mK + mZ
+            MsgBox(mZ)
+            MsgBox(mK)
+        Loop Until mK > 2 * conStartK
+    End Sub
+End Class
